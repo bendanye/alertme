be5dfc2d005641235e3e2e9a0d9f89bb1a9d2743
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2022-02-22 08:57:46 +0800

    Add files via upload

---
+++ alertme.py
@@ -0,0 +1,40 @@
+from playsound import playsound
+import threading
+
+import PySimpleGUI as sg
+
+# pip install playsound==1.2.2
+# pip install -U PyObjC
+# mixkit-cartoon-positive-sound-2255.wav
+
+def alert():
+    loop_thread = threading.Thread(target=__loop_sound, name='backgroundMusicThread')
+    loop_thread.daemon = True
+    loop_thread.start()
+        
+    sg.theme('BluePurple')
+    
+    layout = [[sg.Text('Your Script/Codes have completed!'),
+            sg.Text(size=(15,1), key='-OUTPUT-')],
+            [sg.Button('OK', bind_return_key=True)]]
+    
+    window = sg.Window('Alert Me', layout, return_keyboard_events=True)
+    
+    while True:
+        event, values = window.read()
+
+        if 'Return' in event:
+            break
+        
+        if event in ('None', 'OK'):
+            break
+    
+    window.close()
+
+def __loop_sound():
+    while True:
+        playsound('sound.wav', block=True)
+
+
+if __name__ == "__main__":
+    alert()
