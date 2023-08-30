f7c26036e9705118000c0918c412f86f763cdfeb
bendanye <shen_soonng@hotmail.com>
2023-08-30 14:10:28 +0800

    Black format

--- alertme.py
+++ alertme.py
@@ -5,35 +5,42 @@ import threading
 
 import PySimpleGUI as sg
 
+
 def alert():
-    loop_thread = threading.Thread(target=__loop_sound, name='backgroundMusicThread')
+    loop_thread = threading.Thread(target=__loop_sound, name="backgroundMusicThread")
     loop_thread.daemon = True
     loop_thread.start()
-        
-    sg.theme('BluePurple')
-    
-    layout = [[sg.Text('Your Script/Codes have completed!'),
-            sg.Text(size=(15,1), key='-OUTPUT-')],
-            [sg.Button('OK', bind_return_key=True)]]
-    
-    window = sg.Window('Alert Me', layout, return_keyboard_events=True)
-    
+
+    sg.theme("BluePurple")
+
+    layout = [
+        [
+            sg.Text("Your Script/Codes have completed!"),
+            sg.Text(size=(15, 1), key="-OUTPUT-"),
+        ],
+        [sg.Button("OK", bind_return_key=True)],
+    ]
+
+    window = sg.Window("Alert Me", layout, return_keyboard_events=True)
+
     while True:
         event, values = window.read()
 
-        if 'Return' in event:
+        if "Return" in event:
             break
-        
-        if event in ('None', 'OK'):
+
+        if event in ("None", "OK"):
             break
-    
+
     window.close()
+
 
 def __loop_sound():
     while True:
         playsound(
             f"{os.path.realpath(os.path.dirname(__file__))}/sound.wav", block=True
         )
+
 
 if __name__ == "__main__":
     alert()
